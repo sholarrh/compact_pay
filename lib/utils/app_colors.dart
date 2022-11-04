f8293ff996628ba8be878af94fa922017357e268
sholarrh <adigun.solafunmi@gmail.com>
2022-11-04 03:32:25 -0700

    3rd commit

--- lib/utils/app_colors.dart
+++ lib/utils/app_colors.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-const mainBlue = Color(0xFF5771F9);
+const mainBlue = Color(0xFF2488B0);
 const lightMainBlue = Color(0xFF314BCE);
 const white = Color(0xFFFFFFFF);
 const darkFont = Color(0xFF151940);
@@ -8,6 +8,5 @@ const cardColor = Color(0xFFF5F6FA);
 const orange = Color(0xFFFF897E);
 const oddBlue = Color(0xFF486484);
 const lightGrey = Color(0xFF7F8192);
-const mainBlue1 = Color(0xff7A7AFD);
-const mainBlack = Colors.black;
+const black = Colors.black;
 const deepBlue = Color(0xff151940);
