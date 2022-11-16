f805b9c0fc7988c5d401982d88e43a80a4bad912
Ade556 <ade556@gmail.com>
2022-11-16 20:38:38 +0100

    Third push

--- lib/screens/changepassword.dart
+++ lib/screens/changepassword.dart
@@ -14,10 +14,12 @@ class ChangePassword extends StatefulWidget {
 }
 
 class _ChangePasswordState extends State<ChangePassword> {
+  bool isLoading = false;
   TextEditingController _Oldpassword = TextEditingController();
   TextEditingController _Newpassword = TextEditingController();
   TextEditingController _Repeatpassword = TextEditingController();
 
+  final formkey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -26,96 +28,110 @@ class _ChangePasswordState extends State<ChangePassword> {
         body: Padding(
           padding: const EdgeInsets.only(left: 10, right: 10),
           child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(top: 10, bottom: 20),
-                  child: Row(
-                    children: [
-                      IconButton(
-                        icon: Icon(Icons.close),
-                        color: white,
-                        iconSize: 17,
-                        onPressed: () {
-                          Navigator.pop(context);
-                        },
-                      ),
-                      MyText(
-                        'Change Password',
-                        color: white,
-                        fontFamily: 'Poppins',
-                        fontSize: 16,
-                        fontWeight: FontWeight.w500,
-                      ),
-                    ],
-                  ),
-                ),
-                MyText('Old Password',
-                    color: white,
-                    fontFamily: 'Poppins',
-                    fontWeight: FontWeight.w400,
-                    fontSize: 13),
-                SizedBox(height: 8),
-                InputField(
-                  hintText: '',
-                  validator: validateFullName,
-                  keyBoardType: TextInputType.name,
-                  isPassword: false,
-                  hasSuffixIcon: true,
-                  inputController: _Oldpassword,
-                ),
-                SizedBox(height: 25),
-                MyText('New Password',
-                    color: white,
-                    fontFamily: 'Poppins',
-                    fontWeight: FontWeight.w400,
-                    fontSize: 13),
-                SizedBox(height: 8),
-                InputField(
-                  hintText: '',
-                  validator: validateFullName,
-                  keyBoardType: TextInputType.name,
-                  isPassword: false,
-                  hasSuffixIcon: true,
-                  inputController: _Newpassword,
-                ),
-                SizedBox(height: 25),
-                MyText('Repeat Password',
-                    color: white,
-                    fontFamily: 'Poppins',
-                    fontWeight: FontWeight.w400,
-                    fontSize: 13),
-                SizedBox(height: 8),
-                InputField(
-                  hintText: '',
-                  validator: validateFullName,
-                  keyBoardType: TextInputType.name,
-                  isPassword: false,
-                  hasSuffixIcon: true,
-                  inputController: _Repeatpassword,
-                ),
-                SizedBox(height: 80),
-                MyButton(
-                  child: Container(
-                    height: 54,
-                    width: double.infinity,
-                    decoration: BoxDecoration(
-                      color: mainBlue,
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: Center(
-                      child: MyText(
-                        'Done',
-                        color: white,
-                        fontFamily: 'Poppins',
-                        fontSize: 16,
-                        fontWeight: FontWeight.w600,
-                      ),
+            child: Form(
+              key: formkey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(top: 10, bottom: 20),
+                    child: Row(
+                      children: [
+                        IconButton(
+                          icon: Icon(Icons.close),
+                          color: white,
+                          iconSize: 17,
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                        ),
+                        MyText(
+                          'Change Password',
+                          color: white,
+                          fontFamily: 'Poppins',
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ],
                     ),
                   ),
-                )
-              ],
+                  MyText('Old Password',
+                      color: white,
+                      fontFamily: 'Poppins',
+                      fontWeight: FontWeight.w400,
+                      fontSize: 13),
+                  SizedBox(height: 8),
+                  InputField(
+                    hintText: '',
+                    validator: validatePassword,
+                    keyBoardType: TextInputType.name,
+                    isPassword: false,
+                    hasSuffixIcon: true,
+                    inputController: _Oldpassword,
+                  ),
+                  SizedBox(height: 25),
+                  MyText('New Password',
+                      color: white,
+                      fontFamily: 'Poppins',
+                      fontWeight: FontWeight.w400,
+                      fontSize: 13),
+                  SizedBox(height: 8),
+                  InputField(
+                    hintText: '',
+                    validator: validatePassword,
+                    keyBoardType: TextInputType.name,
+                    isPassword: false,
+                    hasSuffixIcon: true,
+                    inputController: _Newpassword,
+                  ),
+                  SizedBox(height: 25),
+                  MyText('Repeat Password',
+                      color: white,
+                      fontFamily: 'Poppins',
+                      fontWeight: FontWeight.w400,
+                      fontSize: 13),
+                  SizedBox(height: 8),
+                  InputField(
+                    hintText: '',
+                    validator: validatePassword,
+                    keyBoardType: TextInputType.name,
+                    isPassword: false,
+                    hasSuffixIcon: true,
+                    inputController: _Repeatpassword,
+                  ),
+                  SizedBox(height: 80),
+                  MyButton(
+                    onTap: () {
+                      if (formkey.currentState!.validate()) {
+                        isLoading = true;
+                        setState(() {});
+                        Future.delayed(Duration(seconds: 10))
+                            .then((value) async {
+                          isLoading = false;
+                          setState(() {});
+                        });
+                      }
+                    },
+                    child: Container(
+                      height: 54,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        color: mainBlue,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Center(
+                        child: MyText(
+                          'Done',
+                          color: white,
+                          fontFamily: 'Poppins',
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
         ),
